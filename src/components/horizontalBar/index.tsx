e1aeb5ec91210a88dd24a70737347ce2c724763f
alexis <alexisthual@gmail.com>
2020-11-22 21:51:24 +0100

    Padding left and gray ticks for HorizontalBar

--- src/components/horizontalBar/index.tsx
+++ src/components/horizontalBar/index.tsx
@@ -16,7 +16,8 @@ interface Props {
 const HorizontalBar = ({labels, values, width, height}: Props) => {
   const marginLeft = 150
   const paddingTop = 30
-  const xMax = width - marginLeft
+  const marginRight = 30
+  const xMax = width - marginLeft - marginRight
 
 
   const xScale = scaleLinear<number>({
@@ -32,51 +33,64 @@ const HorizontalBar = ({labels, values, width, height}: Props) => {
     padding: 0.2,
   })
 
+  const darkGray5 = '#394B59';
+  const gray1 = '#5C7080';
+  const gray3 = '#8A9BA8';
+  const axisColor = gray3;
 
   return (
     <svg width={width} height={height}>
-      <AxisTop
-        left={marginLeft}
-        top={paddingTop}
-        scale={xScale}
-      />
-      <Group left={marginLeft} top={paddingTop}>
-        {values.map((value, index) => {
-          const label = labels[index]
-          const barHeight = yScale.bandwidth();
-          const barY = yScale(label)
-          const delta = xScale(value) - xScale(0);
-          const barWidth = delta > 0 ? delta : -delta
-          const barX = delta > 0 ? xScale(0) : xScale(value)
+      <Group width={width-marginRight}>
+        <AxisTop
+          left={marginLeft}
+          top={paddingTop}
+          scale={xScale}
+          stroke={axisColor}
+          tickStroke={axisColor}
+          tickLabelProps={() => ({
+            fill: axisColor,
+            fontSize: 11,
+            textAnchor: 'middle',
+          })}
+        />
+        <Group left={marginLeft} top={paddingTop}>
+          {values.map((value, index) => {
+            const label = labels[index]
+            const barHeight = yScale.bandwidth();
+            const barY = yScale(label)
+            const delta = xScale(value) - xScale(0);
+            const barWidth = delta > 0 ? delta : -delta
+            const barX = delta > 0 ? xScale(0) : xScale(value)
 
-          return (
-            <Bar
-              key={`bar-${label}`}
-              x={barX}
-              y={barY}
-              width={barWidth}
-              height={barHeight}
-              fill="rgba(23, 233, 217, .5)"
-              onClick={() => {
-                alert(`clicked: ${label}`);
-              }}
-            />
-          );
-        })}
-      </Group>
-      <Group top={paddingTop}>
-        {labels.map((label, index) => {
-          return (
-            <Text
-              fontSize={8}
-              key={`label-${label}`}
-              textAnchor="end"
-              verticalAnchor="middle"
-              y={yScale(label)}
-              x={marginLeft - 20}
-            >{label}</Text>
-          );
-        })}
+            return (
+              <Bar
+                key={`bar-${label}`}
+                x={barX}
+                y={barY}
+                width={barWidth}
+                height={barHeight}
+                fill="rgba(23, 233, 217, .5)"
+                onClick={() => {
+                  alert(`clicked: ${label}`);
+                }}
+              />
+            );
+          })}
+        </Group>
+        <Group top={paddingTop}>
+          {labels.map((label, index) => {
+            return (
+              <Text
+                fontSize={8}
+                key={`label-${label}`}
+                textAnchor="end"
+                verticalAnchor="middle"
+                y={yScale(label)}
+                x={marginLeft - 20}
+              >{label}</Text>
+            );
+          })}
+        </Group>
       </Group>
     </svg>
   );
