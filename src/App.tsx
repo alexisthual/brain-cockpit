8c6b45c8d9ad7624218d192cbb1234e9e5001eed
alexis <alexisthual@gmail.com>
2020-11-22 21:31:27 +0100

    Connect react application to eel server

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Scene from './Scene';
+export const eel = window.eel
+eel.set_host('ws://localhost:8080')
+eel.server_log('In da cockpit')
 
 class App extends Component {
   render() {
