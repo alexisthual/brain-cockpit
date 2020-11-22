be032e228ee1009aa7b697012ede679c6aee669b
alexis <alexisthual@gmail.com>
2020-11-22 21:33:11 +0100

    Define App state and initialise it

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,33 @@ export const eel = window.eel
 eel.set_host('ws://localhost:8080')
 eel.server_log('In da cockpit')
 
+interface IAppState {
+  labels: string[]
+  contrasts: number[]
+}
+
 class App extends Component {
+  public state: IAppState = {
+    labels: [],
+    contrasts: [],
+  }
+
+  componentDidMount() {
+    eel.get_contrast_labels()((labels: string[]) => {
+      this.setState({
+        labels
+      })
+    })
+  }
+
+  public updateContrasts = (voxelIndex: number) => {
+    eel.explore_voxel(voxelIndex)((contrasts: number[]) => {
+      this.setState({
+        contrasts
+      })
+    })
+  }
+
   render() {
     return (
       <div>
