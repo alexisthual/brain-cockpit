567e10b078f34a929104dba95f7d6ec9200ad8c5
alexis <alexisthual@gmail.com>
2020-11-22 21:34:51 +0100

    Implement App css grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Scene from './Scene';
 import ParentSize from '@visx/responsive/lib/components/ParentSize';
 
 import HorizontalBar from './components/horizontalBar'
+import './App.css'
+
 export const eel = window.eel
 eel.set_host('ws://localhost:8080')
 eel.server_log('In da cockpit')
