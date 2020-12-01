ef3fdfd6489fa30c27cb28ee3d80f114e8c45ea9
alexis <alexisthual@gmail.com>
2020-12-01 23:20:50 +0100

    If subjectLabels undefined, plot subject.label only in header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -44,35 +44,39 @@ const Header = ({
 
   return (
     <div id="header">
-      {subjectLabels ? (
+      {subjectLabels || subject ? (
         <>
           <div className="header-item-label">Subject</div>
           <div className="header-item-value">
-            <ButtonGroup>
-              <SelectSubject
-                disabled={meanContrastMap}
-                filterable={false}
-                items={subjectLabels}
-                itemRenderer={subjectRenderer}
-                onItemSelect={(item: string) => {
-                  console.log(item);
-                  subjectChangeCallback(subjectLabels.indexOf(item));
-                }}
-              >
-                <Button
+            {subjectLabels ? (
+              <ButtonGroup>
+                <SelectSubject
                   disabled={meanContrastMap}
-                  rightIcon="double-caret-vertical"
-                  text={subject}
+                  filterable={false}
+                  items={subjectLabels}
+                  itemRenderer={subjectRenderer}
+                  onItemSelect={(item: string) => {
+                    console.log(item);
+                    subjectChangeCallback(subjectLabels.indexOf(item));
+                  }}
+                >
+                  <Button
+                    disabled={meanContrastMap}
+                    rightIcon="double-caret-vertical"
+                    text={subject}
+                  />
+                </SelectSubject>
+                <Button
+                  active={meanContrastMap}
+                  icon={meanContrastMap ? "ungroup-objects" : "group-objects"}
+                  onClick={meanChangeCallback}
+                  outlined
+                  title={"Take subjects' mean"}
                 />
-              </SelectSubject>
-              <Button
-                active={meanContrastMap}
-                icon={meanContrastMap ? "ungroup-objects" : "group-objects"}
-                onClick={meanChangeCallback}
-                outlined
-                title={"Take subjects' mean"}
-              />
-            </ButtonGroup>
+              </ButtonGroup>
+            ) : (
+              <>{subject}</>
+            )}
           </div>
         </>
       ) : null}
