9f76616e3dd3084af7cb6b5a9a9d02f71ea42998
Mathias Sablé-Meyer <mat-git@s-m.ac>
2020-12-08 12:51:16 +0100

    Get rid of plotly to draw directly on a canvas

--- src/components/slices/index.tsx
+++ src/components/slices/index.tsx
@@ -1,8 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, RefObject, createRef } from "react";
 
 import { eel } from "App";
-
-import Plot from "react-plotly.js";
 
 import "./style.scss";
 
@@ -25,9 +23,107 @@ interface ISlicesState {
   sagital: number[][];
   coronal: number[][];
   horizontal: number[][];
-  index_sagital: number;
-  index_coronal: number;
-  index_horizontal: number;
+}
+
+interface ICanvasSliceProps {
+  anat: number[][];
+  id: string;
+  crosshair: [number, number];
+  clickCallback?: any;
+}
+
+class CanvasSlice extends Component<ICanvasSliceProps, {}> {
+  anat: number[][];
+  clickCallback?: any;
+  id: string;
+  crosshair: [number, number];
+  canvasRef: RefObject<HTMLCanvasElement>;
+
+  constructor(props: ICanvasSliceProps) {
+    super(props);
+    this.anat = this.props.anat;
+    this.canvasRef = createRef<HTMLCanvasElement>();
+    this.clickCallback = this.props.clickCallback;
+    this.id = this.props.id;
+    this.crosshair = this.props.crosshair;
+  }
+
+  componentDidMount() {
+    this.updateHeatmap(this.props.anat, this.props.crosshair);
+  }
+  componentDidUpdate() {
+    this.updateHeatmap(this.props.anat, this.props.crosshair);
+  }
+
+  updateHeatmap(anat: number[][], crosshair: [number, number]) {
+    const canvas = this.canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      const [height, width] = [canvas.clientHeight, canvas.clientWidth];
+      canvas.height = height;
+      canvas.width = width;
+      const size = Math.min(height, width);
+      if (ctx) {
+        ctx.clearRect(0, 0, width, height);
+        const numVox_x = anat.length;
+        const numVox_y = anat[0].length;
+        let range_min = Number.MAX_VALUE;
+        let range_max = Number.MIN_VALUE;
+        const rect_width = size / numVox_x;
+        const rect_height = size / numVox_y;
+        // Determine range for max contrast
+        for (let i = 0; i < numVox_x; i++) {
+          for (let j = 0; j < numVox_y; j++) {
+            if (anat[i][j] < range_min) {
+              range_min = anat[i][j];
+            }
+            if (anat[i][j] > range_max) {
+              range_max = anat[i][j];
+            }
+          }
+        }
+        // Paint the brain
+        for (let i = 0; i < numVox_x; i++) {
+          for (let j = 0; j < numVox_y; j++) {
+            const color = Math.floor(
+              (255 * (anat[i][j] - range_min)) / (range_max - range_min)
+            );
+            ctx.fillStyle = "rgb(" + color + "," + color + "," + color + ")";
+            ctx.fillRect(
+              i * rect_width,
+              size - j * rect_height,
+              rect_width,
+              rect_height
+            );
+          }
+        }
+        // Paint the crosshair
+        ctx.fillStyle = "rgba(50, 50, 255, 0.5";
+        ctx.fillRect(
+          crosshair[0] * rect_width + rect_width / 4,
+          0,
+          rect_width / 2,
+          size
+        );
+        ctx.fillRect(
+          0,
+          size - crosshair[1] * rect_height + rect_height / 4,
+          size,
+          rect_height / 2
+        );
+        canvas.onclick = (e) => {
+          this.clickCallback([
+            Math.floor(e.offsetX / rect_width),
+            Math.floor((size - e.offsetY) / rect_height),
+          ]);
+        };
+      }
+    }
+  }
+
+  render() {
+    return <canvas ref={this.canvasRef} id={this.id} />;
+  }
 }
 
 class Slices extends Component<ISlicesProps, ISlicesState> {
@@ -38,35 +134,16 @@ class Slices extends Component<ISlicesProps, ISlicesState> {
   subject: string;
   mni: [number, number, number];
   slices: [number, number, number];
-  defaultLayout: Object;
 
   constructor(props: ISlicesProps) {
     super(props);
-    this.state = {
-      sagital: [[]],
-      coronal: [[]],
-      horizontal: [[]],
-      index_coronal: this.props.slices[0],
-      index_sagital: this.props.slices[1],
-      index_horizontal: this.props.slices[2],
-    };
+    this.state = { sagital: [[]], coronal: [[]], horizontal: [[]] };
     this.contrast = this.props.contrast;
     this.tThreshold = this.props.tThreshold;
     this.task = this.props.task;
     this.subject = this.props.subject;
     this.mni = this.props.mni;
     this.slices = this.props.slices;
-    this.defaultLayout = {
-      margin: { l: 0, r: 0, b: 0, t: 0 },
-      yaxis: { scaleanchor: "x", scaleratio: 1 },
-      plot_bgcolor: "#000000",
-    };
-  }
-
-  plotlyDataFromData(data: number[][]) {
-    return [
-      { z: data, type: "heatmap", colorscale: "Greys", showscale: false },
-    ];
   }
 
   componentDidMount() {
@@ -88,9 +165,7 @@ class Slices extends Component<ISlicesProps, ISlicesState> {
   }
 
   update_coronal(slice: number) {
-    const tic = performance.now();
     eel.get_slice_coronal(slice)((slices_values: number[][]) => {
-      const toc = performance.now();
       this.setState({ coronal: slices_values });
     });
   }
@@ -112,8 +187,8 @@ class Slices extends Component<ISlicesProps, ISlicesState> {
     this.update_horizontal(this.slices[2]);
   }
 
-  handle_click(who: Cuts, event: any) {
-    const [clicked_y, clicked_x] = event.points[0].pointIndex;
+  handle_click(who: Cuts, event: [number, number]) {
+    const [clicked_x, clicked_y] = event;
     if (who === Cuts.Sagital) {
       console.log("sagital");
       this.slices[1] = clicked_x;
@@ -183,20 +258,29 @@ class Slices extends Component<ISlicesProps, ISlicesState> {
   render() {
     return (
       <>
-        <Plot
-          data={this.plotlyDataFromData(this.state.sagital) as any}
-          layout={this.defaultLayout as any}
-          onClick={(e) => this.handle_click(Cuts.Sagital, e)}
+        <CanvasSlice
+          anat={this.state.sagital}
+          crosshair={[this.slices[1], this.slices[2]]}
+          id={"sagital"}
+          clickCallback={(e: [number, number]) =>
+            this.handle_click(Cuts.Sagital, e)
+          }
         />
-        <Plot
-          data={this.plotlyDataFromData(this.state.coronal) as any}
-          layout={this.defaultLayout as any}
-          onClick={(e) => this.handle_click(Cuts.Coronal, e)}
+        <CanvasSlice
+          anat={this.state.coronal}
+          crosshair={[this.slices[0], this.slices[2]]}
+          id={"coronal"}
+          clickCallback={(e: [number, number]) =>
+            this.handle_click(Cuts.Coronal, e)
+          }
         />
-        <Plot
-          data={this.plotlyDataFromData(this.state.horizontal) as any}
-          layout={this.defaultLayout as any}
-          onClick={(e) => this.handle_click(Cuts.Horizontal, e)}
+        <CanvasSlice
+          anat={this.state.horizontal}
+          crosshair={[this.slices[0], this.slices[1]]}
+          id={"horizontal"}
+          clickCallback={(e: [number, number]) =>
+            this.handle_click(Cuts.Horizontal, e)
+          }
         />
       </>
     );
