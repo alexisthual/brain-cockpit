def1b4048dec9c320c24524285e8d3f3e5c44940
alexis <alexisthual@gmail.com>
2020-12-17 20:15:10 +0100

    Remove useless useeffect in canvasSlice

--- src/components/canvasSlice/index.tsx
+++ src/components/canvasSlice/index.tsx
@@ -1,6 +1,6 @@
 import { Colors } from "@blueprintjs/core";
 import interpolate from "color-interpolate";
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 
 import "./style.scss";
 
@@ -40,56 +40,44 @@ const CanvasSlice = ({
   const canvasEl = useRef<HTMLCanvasElement>(null);
   const colormap = interpolate([color1, color2]);
 
-  useEffect(() => {
-    const canvas = canvasEl.current;
-    const ctx = canvas?.getContext("2d");
+  const canvas = canvasEl.current;
+  const ctx = canvas?.getContext("2d");
 
-    if (canvas && ctx && image.length > 0) {
-      canvas.height = height;
-      canvas.width = width;
-      const pixelWidth = width / image.length;
-      const pixelHeight = height / image[0].length;
-      const max = getMax(image);
-      const min = getMin(image, threshold ? max * threshold : 0);
+  if (canvas && ctx && image.length > 0) {
+    canvas.height = height;
+    canvas.width = width;
+    const pixelWidth = width / image.length;
+    const pixelHeight = height / image[0].length;
+    const max = getMax(image);
+    const min = getMin(image, threshold ? max * threshold : 0);
 
-      // Draw image pixels
-      for (let i = 0; i < image.length; i++) {
-        for (let j = 0; j < image[0].length; j++) {
-          const intensity = image[i][j];
-          const normalizedIntensity = Math.min(
-            Math.max((intensity - min) / (max - min), 0),
-            1
-          );
+    // Draw image pixels
+    for (let i = 0; i < image.length; i++) {
+      for (let j = 0; j < image[0].length; j++) {
+        const intensity = image[i][j];
+        const normalizedIntensity = Math.min(
+          Math.max((intensity - min) / (max - min), 0),
+          1
+        );
 
-          ctx.fillStyle =
-            threshold && Math.abs(intensity) < threshold * max
-              ? colorString.to.rgb(colorString.get.rgb(color1).slice(0, 3), 0)
-              : colorString.to.rgb(
-                  colorString.get
-                    .rgb(colormap(normalizedIntensity || 0))
-                    .slice(0, 3),
-                  max - min > 0 ? alpha : 0
-                );
-          ctx.fillRect(
-            i * pixelWidth,
-            j * pixelHeight,
-            1.2 * pixelWidth,
-            1.2 * pixelHeight
-          );
-        }
+        ctx.fillStyle =
+          threshold && Math.abs(intensity) < threshold * max
+            ? colorString.to.rgb(colorString.get.rgb(color1).slice(0, 3), 0)
+            : colorString.to.rgb(
+                colorString.get
+                  .rgb(colormap(normalizedIntensity || 0))
+                  .slice(0, 3),
+                max - min > 0 ? alpha : 0
+              );
+        ctx.fillRect(
+          i * pixelWidth,
+          j * pixelHeight,
+          1.2 * pixelWidth,
+          1.2 * pixelHeight
+        );
       }
     }
-  }, [
-    image,
-    width,
-    height,
-    canvasEl,
-    alpha,
-    threshold,
-    colormap,
-    color1,
-    color2,
-  ]);
+  }
 
   return <canvas ref={canvasEl} className={"slice-canvas"} />;
 };
