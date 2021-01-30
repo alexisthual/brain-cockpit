49862d8c4cebee141bf747e85d237731288cd2cb
alexisthual <alexisthual@gmail.com>
2021-01-30 14:05:39 +0100

    Show view links depending on .env configuration

--- src/App.tsx
+++ src/App.tsx
@@ -32,52 +32,65 @@ const App = () => {
     <Router>
       <div id="app-container">
         <div id="navbar">
-          <Tooltip
-            content="IBC conditions on surface"
-            position={Position.RIGHT}
-          >
-            <NavLink className="view-button" exact to="/">
-              <Icon icon="symbol-square" />
-            </NavLink>
-          </Tooltip>
-          <Tooltip content="IBC conditions slices" position={Position.RIGHT}>
-            <NavLink className="view-button" exact to="/cuts">
-              <Icon icon="symbol-circle" />
-            </NavLink>
-          </Tooltip>
-          <Tooltip content="IBC conditions volume" position={Position.RIGHT}>
-            <NavLink className="view-button" to="/volume">
-              <Icon icon="cube" />
-            </NavLink>
-          </Tooltip>
+          {process.env.REACT_APP_CONDITIONS_VIEW ? (
+            <Tooltip
+              content="IBC conditions on surface"
+              position={Position.RIGHT}
+            >
+              <NavLink className="view-button" exact to="/">
+                <Icon icon="symbol-square" />
+              </NavLink>
+            </Tooltip>
+          ) : null}
           <Divider />
-          <Tooltip
-            content="Regressed MNI coordinates"
-            position={Position.RIGHT}
-          >
-            <NavLink className="view-button" to="/regression">
-              <Icon icon="regression-chart" />
-            </NavLink>
-          </Tooltip>
-          <Tooltip content="Functional distance maps" position={Position.RIGHT}>
-            <NavLink className="view-button" to="/functional-distance">
-              <Icon icon="heatmap" />
-            </NavLink>
-          </Tooltip>
+          {process.env.REACT_APP_SLICE_VIEW ? (
+            <Tooltip content="Cross-species slices" position={Position.RIGHT}>
+              <NavLink className="view-button" exact to="/cuts">
+                <Icon icon="symbol-circle" />
+              </NavLink>
+            </Tooltip>
+          ) : null}
+          <Divider />
+          {process.env.REACT_APP_EXPERIMENT_REGRESSION_VIEW ? (
+            <Tooltip
+              content="Regressed MNI coordinates"
+              position={Position.RIGHT}
+            >
+              <NavLink className="view-button" to="/regression">
+                <Icon icon="regression-chart" />
+              </NavLink>
+            </Tooltip>
+          ) : null}
+          {process.env.REACT_APP_EXPERIMENT_CORRELATION_VIEW ? (
+            <Tooltip
+              content="Functional distance maps"
+              position={Position.RIGHT}
+            >
+              <NavLink className="view-button" to="/functional-distance">
+                <Icon icon="heatmap" />
+              </NavLink>
+            </Tooltip>
+          ) : null}
         </div>
         <Switch>
           <Route exact path="/">
             <SurfaceExplorer />
           </Route>
-          <Route exact path="/cuts">
-            <CutsExplorer />
-          </Route>
-          <Route exact path="/regression">
-            <RegressionExplorer />
-          </Route>
-          <Route exact path="/functional-distance">
-            <FunctionalDistanceExplorer />
-          </Route>
+          {process.env.REACT_APP_SLICE_VIEW ? (
+            <Route exact path="/cuts">
+              <CutsExplorer />
+            </Route>
+          ) : null}
+          {process.env.REACT_APP_EXPERIMENT_REGRESSION_VIEW ? (
+            <Route exact path="/regression">
+              <RegressionExplorer />
+            </Route>
+          ) : null}
+          {process.env.REACT_APP_EXPERIMENT_CORRELATION_VIEW ? (
+            <Route exact path="/functional-distance">
+              <FunctionalDistanceExplorer />
+            </Route>
+          ) : null}
           <Route>
             <NonIdealState
               icon={"warning-sign"}
