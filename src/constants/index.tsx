777771d3afcf903fccc7a825f72b1d1479b9c560
alexis <alexisthual@gmail.com>
2021-03-30 17:58:56 +0200

    Remove padding for diverging color scales

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -36,12 +36,10 @@ export const colormaps = {
     // .bezier(["#e66101", "#fdb863", "#d4d0d0", "#b2abd2", "#5e3c99"])
     .bezier(["#7b3294", "#c2a5cf", "#d4d0d0", "#a6dba0", "#008837"])
     .scale()
-    .padding(-0.3)
     .mode("lab"),
   diverging_temperature: chroma
     .bezier(["#0571b0", "#92c5de", "#c4c4c4", "#f4a582", "#ca0020"])
     .scale()
-    .padding(-0.3)
     .mode("lab"),
 };
 
