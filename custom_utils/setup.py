2aac58645b6c0bf2ae291acc8eacfe76c2a592b3
alexis <alexisthual@gmail.com>
2021-08-04 17:43:51 +0200

    Better logging when loading env files

--- custom_utils/setup.py
+++ custom_utils/setup.py
@@ -20,22 +20,26 @@ def load_env(verbose=False):
 
     dotenv.load_dotenv()
     if args.env == "production":
-        if verbose:
-            print("Loading production env files...")
         if os.path.exists(".env.production"):
             dotenv.load_dotenv(dotenv_path=".env.production", override=True)
+            if verbose:
+                print("Loaded .env.production")
         if os.path.exists(".env.production.local"):
             dotenv.load_dotenv(
                 dotenv_path=".env.production.local", override=True
             )
+            if verbose:
+                print("Loaded .env.production.local")
     else:
-        if verbose:
-            print("Loading development env files...")
         if os.path.exists(".env.development"):
             dotenv.load_dotenv(dotenv_path=".env.development", override=True)
+            if verbose:
+                print("Loaded .env.development.local")
         if os.path.exists(".env.development.local"):
             dotenv.load_dotenv(
                 dotenv_path=".env.development.local", override=True
             )
+            if verbose:
+                print("Loaded .env.development.local")
 
     return args.env
